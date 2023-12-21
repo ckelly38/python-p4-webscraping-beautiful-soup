13167b576514435fbee7298ad1c318035d212460
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-12-21 02:00:51 -0700

    this is an example of scraping a website

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,26 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser');
+
+print(doc);
+
+print(doc.select('.visually-hidden')[0].contents);#heading-financier
+
+#.heading-25-extrabold
+
+mytitleobjs = doc.select('.heading-25-extrabold');
+
+print(mytitleobjs);
+
+#len(mytitleobjs)
+for n in range(4):
+    #print(mytitleobjs[n]);
+    #print(mytitleobjs[n].contents);
+    print(mytitleobjs[n].contents[0].strip());
+    #print(type(mytitleobjs[n].contents[0]));
+    #print(mytitleobjs[n].contents.strip);
+
+print(mytitleobjs[0].name);
+print(mytitleobjs[0].attrs);
